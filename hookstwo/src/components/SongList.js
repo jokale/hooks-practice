8606883b818b8553c69502fdbf1a351af4b27054
jokale <kalemajoanna@gmail.com>
2021-05-12 15:10:11 +0100

    create new hooks directory

---
+++ hookstwo/src/components/SongList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>superbass</li>
+                <li>roman revenge</li>
+                <li>massive attack</li>
+
+            </ul>
+
+        </div>
+      );
+}
+ 
+export default SongList;
